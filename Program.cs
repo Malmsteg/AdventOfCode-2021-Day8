e0bf044df9d9a3fc7dab56ab9bd21a300adf4f77
Malmsteg <73699667+Malmsteg@users.noreply.github.com>
2021-12-10 16:28:20 +0100

    Done!

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ namespace AdventOfCode_2021_Day8
             for(int i = 1; i < splitted.Length; i+=2)
             {
                 int k = (i-1) * 2;
-                string[] temp = splitted[i].Split(" "); // i = 1 -> 0,1,2,3; i = 3 -> 4,5,6,7; i = 5 -> 8,9,10,11 
+                string[] temp = splitted[i].Split(" ");
                 rightSide[k] = temp[0];
                 rightSide[k + 1] = temp[1];
                 rightSide[k + 2] = temp[2];
@@ -40,6 +40,129 @@ namespace AdventOfCode_2021_Day8
                 }
             }
             Console.WriteLine("Part 1: " + result);
-          }
+
+            // Part 2
+            int part2Res = 0;
+            for(int i = 0; i < splitted.Length; i+=2)
+            {
+                List<string> leftSide = splitted[i].Split(" ").ToList();
+                List<string> rightList = splitted[i+1].Split(" ").ToList();
+                for(int j = 0; j < rightList.Count; j++)
+                {
+                    char[] c = rightList[j].ToArray();
+                    Array.Sort(c);
+                    rightList[j] = new string(c);
+                }
+                Dictionary<int,string> solver = new();
+                for( int j = 0; j < leftSide.Count; j++)
+                {
+                    if(leftSide[j].Length == 2){
+                        solver[1] = leftSide[j];
+                    }
+                    if(leftSide[j].Length == 3){
+                        solver[7] = leftSide[j];
+                    }
+                    if(leftSide[j].Length == 4){
+                        solver[4] = leftSide[j];
+                    }
+                    if(leftSide[j].Length == 7){
+                        solver[8] = leftSide[j];
+                    }
+                }
+
+                for(int j = 0; j < leftSide.Count; j++)
+                {
+                    if(leftSide[j].Length==5)
+                    {
+                        //3
+                        if(leftSide[j].Contains(solver[1][0])
+                        && leftSide[j].Contains(solver[1][1]))
+                        {
+                            solver[3] = leftSide[j];
+                            break;
+                        }
+                    }
+                }
+                //2 & 5
+                for(int j = 0; j < leftSide.Count; j++)
+                {
+                    if(leftSide[j].Length == 5)
+                    {
+                        int count = 0;
+                        for(int k = 0; k < 4; k++){
+                            if(leftSide[j].Contains(solver[4][k]))
+                            {
+                                count++;
+                            }
+                        }
+                        //2
+                        if(count == 2)
+                        {
+                            solver[2] = leftSide[j];
+                        }
+                        //5
+                        else if(count == 3 && !leftSide[j].Equals(solver[3]))
+                        {
+                            solver[5] = leftSide[j];
+                        }
+                    }
+                }
+                // 0, 6 && 9
+                for(int p = 0; p < leftSide.Count; p++)
+                {
+                    if(leftSide[p].Length == 6)
+                    {
+                        //6
+                        if
+                        (
+                            !leftSide[p].Contains(solver[1][0])
+                            || !leftSide[p].Contains(solver[1][1])
+                        )
+                        {
+                            solver[6] = leftSide[p];
+                        }
+                        //0
+                        else if
+                        (
+                            !leftSide[p].Contains(solver[4][0])
+                            || !leftSide[p].Contains(solver[4][1])
+                            || !leftSide[p].Contains(solver[4][2])
+                            || !leftSide[p].Contains(solver[4][3])
+                        )
+                        {
+                            solver[0] = leftSide[p];
+                        }
+                        //9
+                        else
+                        {
+                            solver[9] = leftSide[p];
+                        }
+                    }
+                }
+                for(int j = 0; j < solver.Count; j++)
+                {
+                    char[] c = solver[j].ToArray();
+                    Array.Sort(c);
+                    solver[j] = new string(c);
+                }
+                string res = "";
+                foreach(string item in rightList)
+                {
+                    for(int j = 0; j < solver.Count; j++)
+                    {
+                        if(item.Equals(solver[j]))
+                        {
+                            res+=j.ToString();
+                            break;
+                        }
+                    }
+                }
+                if(!string.IsNullOrEmpty(res))
+                {
+                    part2Res+= Convert.ToInt32(res);
+                }
+            }
+            Console.WriteLine(part2Res);
+        }
     }
 }
